db7752a854de5ff8a9851504f3ffc46b1ec9dca8
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2021-02-28 18:03:45 +0300

    CRUD for VehicleParts implemented

--- WebAutopark/WebAutopark.BusinessLogic/ViewModels/VehiclePartViewModel.cs
+++ WebAutopark/WebAutopark.BusinessLogic/ViewModels/VehiclePartViewModel.cs
@@ -10,6 +10,7 @@ namespace WebAutopark.BusinessLogic.ViewModels
     {
         public int VehiclePartId { get; set; }
         public string Name { get; set; }
+        public int Amount { get; set; }
         public int Price { get; set; }
         public string Description { get; set; }
     }
