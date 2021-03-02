e2408e3842221c1e36a3c5fd5eeec2a7f34c50e7
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2021-03-02 16:00:04 +0300

    All view updated, added viewModelsValidationAttribute

--- WebAutopark/WebAutopark.BusinessLogic/ViewModels/VehicleTypeViewModel.cs
+++ WebAutopark/WebAutopark.BusinessLogic/ViewModels/VehicleTypeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,10 @@ namespace WebAutopark.BusinessLogic.ViewModels
     public class VehicleTypeViewModel
     {
         public int VehicleTypeId { get; set; }
+        [Required]
         public string TypeName { get; set; }
+        [Required]
+        [Range(double.Epsilon, double.MaxValue)]
         public double TaxCoeff { get; set; }
     }
 }
